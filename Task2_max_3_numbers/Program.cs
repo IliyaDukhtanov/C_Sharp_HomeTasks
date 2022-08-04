92ccdcff237762484eab4a75f4b2afb0c2b74c16
Iliya <ilusann@inbox.ru>
2022-08-04 15:01:50 +0300

    HomeTask2 Max from 3 numbers was added

---
+++ Task2_max_3_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (b > a)
+{
+    max = b;
+}
+else max = a;
+if (c > max)
+{
+    max = c;
+}
+else 
+{
+}
+Console.Write("Наибольшее из введённый чисел: ");
+Console.WriteLine(max);
