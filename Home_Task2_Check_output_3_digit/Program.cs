67a36f5f88bed1694e78190bf82077d08f131a04
Iliya <ilusann@inbox.ru>
2022-08-09 15:49:56 +0300

    Добавлены домашние задания по второму семинару (Home_Task1, 2 и 3)

---
+++ Home_Task2_Check_output_3_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+string number = Convert.ToString(Console.ReadLine());
+if (number.Length > 2)
+{
+    Console.WriteLine("Третья цифра введённого числа: " + number[2]);
+}
+else
+{
+    Console.WriteLine("У введённого числа третьей цифры нет");
+}
