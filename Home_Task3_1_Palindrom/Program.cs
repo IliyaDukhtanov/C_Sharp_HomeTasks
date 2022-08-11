7cbb81bd881175e1c7f2d18d9b26e2754431df0c
Iliya <ilusann@inbox.ru>
2022-08-11 14:23:07 +0300

    an universal solution was added

--- Home_Task3_1_Palindrom/Program.cs
+++ Home_Task3_1_Palindrom/Program.cs
@@ -1,6 +1,7 @@
 ﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 является ли оно палиндромом.*/
 
+Console.Clear();
 Console.WriteLine("Введите пятизначное число:");
 string number = Console.ReadLine();
 if (number.Length != 5)
@@ -9,13 +10,13 @@ else
 {
     if (number.Length % 2 == 0)
     {
-        Console.WriteLine("Введённое число - не палиндром");
+        Console.WriteLine("Введённое число не является палиндромом");
     }
     else
     {
         if(number[0] == number [4] & number[1] == number[3])
         {
-            Console.WriteLine("Введённое число - палиндром");
+            Console.WriteLine("Введённое число является палиндром");
         }
         else
         {
@@ -23,3 +24,26 @@ else
         }
     }
 }
+
+// Универсальное решение
+/*Console.WriteLine("Введите слово или число:");
+string value = Console.ReadLine();
+int length = value.Length;
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length/2; i++)
+    while (value[i] == value[length-1])
+    {
+        return (true);    
+    } 
+    return (false);    
+}
+if (IsPalindrome())
+{ 
+    Console.WriteLine("Введное слово/число " + value + " является палиндромом");
+}
+else 
+{
+    Console.WriteLine("Введное слово/число " + value + " является палиндромом");
+}*/
