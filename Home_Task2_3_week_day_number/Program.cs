3ed880f762f5d6c2e3f564ba411fb0bdd5593b72
Iliya <ilusann@inbox.ru>
2022-08-12 11:57:35 +0300

    Добавлен второй вариант решения со словарём

--- Home_Task2_3_week_day_number/Program.cs
+++ Home_Task2_3_week_day_number/Program.cs
@@ -1,6 +1,7 @@
 ﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 и проверяет, является ли этот день выходным*/
 
+// Вариант1
 /*int[] week = new int[7]{1,2,3,4,5,6,7}; // задан массив номеров дней недели
 Console.WriteLine("Введите цифру, обозначающую день недели:");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -13,7 +14,10 @@ else
     Console.WriteLine("Введённый день является выходным");
 }*/
 
-/*Dictionary<int, string> week = new Dictionary<int, string>()
+
+// Вариант 2
+//static void Main(string[] args);
+Dictionary<int, string> week = new Dictionary<int, string>()
 {
     {1, "Понедельник, рабочий день"},
     {2, "Вторник, рабочий день"},
@@ -22,5 +26,7 @@ else
     {5, "Пятница, рабочий день"},
     {6, "Суббота, выходной день"},
     {7, "Воскресенье, выходной день"},
-
-}*/
+};
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(week[day]);
