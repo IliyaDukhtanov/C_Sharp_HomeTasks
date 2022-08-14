14d70db547b16b313f54ce3f7089526e7520deac
Iliya <ilusann@inbox.ru>
2022-08-14 15:43:42 +0300

    add Home_Task3_3_Cube_table_of_digits

---
+++ Home_Task3_3_Cube_table_of_digits/Program.cs
@@ -0,0 +1,86 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Кубы чисел от 1 до " + n + ": ");
+for (int i = 1 ; i < n + 1; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine(cube);
+    }
+return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+
+int[] GetCubeTable(int N)
+{
+    int[] cubs = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+
+void PrintCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{number} ^ 3 = ");
+        System.Console.WriteLine(array[i]);
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+PrintCubs(CubeTable);*/
+
+
+
+/*Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);*/
