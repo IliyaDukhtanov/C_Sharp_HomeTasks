b45d506263e6c78658c4bb20cd40a613dab2d9c6
Iliya <ilusann@inbox.ru>
2022-08-15 14:35:10 +0300

    add Home_Task4_3_Create_Print_array

---
+++ Home_Task4_3_Create_Print_array/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+class Program
+{
+static int[] CreateArray(int number) // создание массива
+{
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
+static void PrintArray(int[] array)  // печать массива
+{
+    Console.WriteLine("Вывод массива");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+static void Main(string[] args)
+{
+    Console.Clear();
+    Console.WriteLine("Введите число элементов массива:");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = CreateArray(length);
+    PrintArray(array);
+}
+}
