4b7f1912b710c86f965b318627766c6a938e587a
Iliya <ilusann@inbox.ru>
2022-08-18 14:49:40 +0300

    add Home_Task5_3_Difference_max_min_array_value

---
+++ Home_Task5_3_Difference_max_min_array_value/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+class Programm 
+{
+static void Main(string[] args)
+{
+    Console.Clear();
+    int[] array = new int[0]; // сначала создаём пустой массив
+    while (true)
+    {
+        Console.WriteLine("Введите значение массива:"); ///// Создание массива произвольной длины в ручную.
+        string meaning = Console.ReadLine();
+        if(meaning != "end")
+        {
+            int number = Convert.ToInt32(meaning);
+            int[] newArray = array;
+            array = new int[array.Length +1];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                array[i] = newArray[i];
+            }
+            array[array.Length-1] = number;
+        }
+        else
+        {
+            break; // конец программы ввода массива в ручную
+        }
+    }  
+    static void PrintResult(int[] array)  // вычисление и печать результата
+    {
+    Console.WriteLine("Вывод массива");
+    int posMax = 0;
+    int posMin = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+            if (array[i] > array[posMax]) posMax = i;
+            else {}
+            if (array[i] < array[posMin]) posMin = i;
+            else {}
+        }
+        System.Console.WriteLine();
+        Console.WriteLine("Разница между Max и Min элементом массива = " + (array[posMax] - array[posMin]));
+        System.Console.WriteLine();
+        //dif = array[posMax] - array[posMin];
+    }
+PrintResult(array);
+}
+}
