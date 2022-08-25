89b54c9fd2178d3f43ce4bb0147f6f95d28c8ca0
Iliya <ilusann@inbox.ru>
2022-08-25 16:18:26 +0300

    Добавлен вариант решения от преподавателя

--- Home_Task6_1_count_of_array_elements/Program.cs
+++ Home_Task6_1_count_of_array_elements/Program.cs
@@ -25,10 +25,31 @@ static void PrintResult(int[] array)  // расчёт и вывод резуль
 }
 
 Console.Clear();
-PrintResult(CreateArray(123)); // массив в 123 элементов
+PrintResult(CreateArray(123)); // массив из 123 элементов
 
 
 // Вариант препода
-/*using Systems; 
- int[] startArray = GetArray(123, 0, 1000); 
- Comsole.WriteLine($"Количество элементов в отрезке [10;99]) = {GetCountElements(start)}
+/*using System; 
+int[] startArray = GetArray(123, 0, 1000); 
+Console.WriteLine($"Количество элементов в отрезке [10;99]) = {GetCountElements(startArray, 10, 99)}");
+
+int[] GetArray(int size, int minValue, int maxValue) // создание массива по входным данным
+{
+    int[] res = new int[size]; 
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
+int GetCountElements(int[] array, int leftRange, int rigthRange)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item >= leftRange && item <= rigthRange) count++;
+    }
+    return count;
+}
+*/
