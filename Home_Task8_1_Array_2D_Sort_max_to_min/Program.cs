7d8deb6822240418992625cc9ce7ead6375f859e
Iliya <ilusann@inbox.ru>
2022-08-29 19:48:29 +0300

    Add Home_Task8_1_Array_2D_Sort_max_to_min. Сортировка значений в строках 2D массива

---
+++ Home_Task8_1_Array_2D_Sort_max_to_min/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void CreateArray(int[,] matrix) // метод создания массива 2D и вывода на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {    
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}    
+
+void FillArray(int[,] matrix) // Метод заполнения массива 2D
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(10);
+        }
+    }
+}
+
+void SortArrayStrings(int[,] matrix) // Метод сортировки значений в строках массива 2D и вывод результата на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int maxPos = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, maxPos]) maxPos = k;
+            }   
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[i, maxPos];  
+            matrix[i, maxPos] = temp;               
+        Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = new int[4, 4];
+
+FillArray(array);
+CreateArray(array);
+Console.WriteLine();
+SortArrayStrings(array);
