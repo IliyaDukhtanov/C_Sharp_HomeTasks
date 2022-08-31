c4ac57ccd40836afc7b9d13a2a9e40e525cf68ae
Iliya <ilusann@inbox.ru>
2022-08-31 16:16:24 +0300

    add Home_Task8_5_Spiral_Fill_Array

---
+++ Home_Task8_5_Spiral_Fill_Array/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+void PrintArray(int[,] matrix)   // метод вывода на экран матрицы 2D
+{
+    const int cellWidth = 3;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],cellWidth}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SpiralFillArray(int[,] matrix) 
+{   
+    int value = 1;
+    int i = 0;
+    int j = 0;
+    while (value <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = value;
+        value++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;   
+}
+
+int n = 4;
+int[,] matrix = new int[n, n];
+Console.Clear();
+Console.WriteLine($"Заполниние массива спирально:");
+Console.WriteLine();
+SpiralFillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
