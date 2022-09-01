3ac6dfd0ec0e483386f47c00d1f31c4181058dc1
Iliya <ilusann@inbox.ru>
2022-09-01 15:39:19 +0300

    add Home_Task9_2_Recursion_output_sum

---
+++ Home_Task9_2_Recursion_output_sum/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int RecursionSum(int m, int n)  //Рекурсивный метод суммирования чисел от M до N
+{
+    if (m > n) 
+    {
+        int temp = m;
+        m = n; 
+        n = temp;
+    }   
+    if (n == m) return n;
+    else return n + RecursionSum(m, n-1);
+}
+
+int InputNumbers(string input)  //  метод для ввода данных пользователем
+{
+  Console.Write(input);
+  int output = int.Parse(Console.ReadLine());
+  return output;
+}
+
+Console.Clear();
+Console.WriteLine();
+int m = InputNumbers("Введите первое число (M): ");
+int n = InputNumbers("Введите второе число (N): ");
+Console.WriteLine();
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {RecursionSum(m, n)}");
+Console.WriteLine();
